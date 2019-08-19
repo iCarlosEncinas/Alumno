598222692c37557034ad9c5870dbec4379ccb0aa
Carlos Encinas <carlos-encinas@hotmail.com>
2019-08-19 12:30:54 -0700

    Agregar archivos de proyecto.

---
+++ Alumno/Alumno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alumno
+{
+    class Alumno
+    {
+        public string Nombre { get; set; }
+        public string Matricula { get; set; }
+        public DateTime FechadeNacimiento { get; set; }
+        public string Dirección { get; set; }
+        public string Telefono { get; set; }
+
+        
+        public Alumno()
+        {
+            Nombre = "Pendiente";
+            Matricula = "Sin Asignar";
+        }
+
+        public Alumno(string nombre , string matricula)
+        {
+            Nombre = nombre;
+            Matricula = matricula;
+        }
+
+    }
+}
---
+++ Alumno/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alumno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Alumno alumno = new Alumno();
+
+            Console.WriteLine("Matrícula:       " + alumno.Matricula);
+            Console.WriteLine("Nombre:          " + alumno.Nombre);
+
+            
+
+            Console.WriteLine("\n\nAlumno 2");
+
+            Alumno alumno1 = new Alumno("Pedro", "189299");
+
+            Console.WriteLine("Matrícula:       " + alumno1.Matricula);
+
+            Console.WriteLine("Nombre:          " + alumno1.Nombre);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
